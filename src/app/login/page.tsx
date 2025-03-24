74dbac16b582724556752155e46544a89ecd2c55
Fook-kiez <pornpanitphanpusit@gmail.com>
2025-03-24 14:12:38 +0700

    login-register

---
+++ src/app/login/page.tsx
@@ -0,0 +1,37 @@
+import DateReserve from "@/components/DateReserve"
+import { TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material"
+import { Dayjs } from "dayjs"
+import Link from "next/link"
+export default function LoginPage() {
+    return (
+<div className="font-mono flex flex-col items-center ">
+            
+            <hr className="w-[100%] " />
+            <div className="text-center flex flex-col items-center justify-center   
+            border-2 rounded-xl p-10 w-[700px] h-[600px] mx-auto my-10 ">
+                <h1 className="text-5xl py-5 font-medium h-[150px] bg-gradient-to-r from-[#4AC9FF] to-[#4AC9FF] 
+                inline-block bg-clip-text text-transparent  ">Login by email</ h1>
+                <div className="flex flex-col items-center">
+                    
+                    <TextField name="Email" label="Email" variant="standard" className="w-[500px] h-[100px]"
+                    fullWidth  /> <br />
+                    <TextField name="Password" label="Password" variant="standard" className="w-[500px] h-[100px]"
+                    fullWidth  />
+
+                    
+                    
+                    
+                    <button className="block rounded-md px-3 py-2
+                    text-white shadow-sm w-[100%] my-2
+                    bg-gradient-to-r from-[#4AC9FF] to-[#4AC9FF]
+                    hover:from-[#4AC9FF] hover:to-[#4AC9FF] transition duration-500 h-[50px] ease-in-out hover:scale-105"
+                    name="Book Venue">
+                        Login
+                    </button>
+                    <Link rel="stylesheet" href="/register" >
+                        <h1 className=" h-[50px]"> don't have account?</h1>
+                    </Link>
+                </div>
+            </div>
+            
+        </div>)}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,37 @@
+import DateReserve from "@/components/DateReserve"
+import { TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material"
+import { Dayjs } from "dayjs"
+import Link from "next/link"
+export default function LoginPage() {
+    return (
+<div className="font-mono flex flex-col items-center ">
+            
+            <hr className="w-[100%] " />
+            <div className="text-center flex flex-col items-center justify-center   
+            border-2 rounded-xl p-10 w-[700px] h-[600px] mx-auto my-10 ">
+                <h1 className="text-5xl py-5 font-medium h-[80px] bg-gradient-to-r from-[#4AC9FF] to-[#4AC9FF] 
+                inline-block bg-clip-text text-transparent  ">Register</h1>
+                <div className="flex flex-col items-center">
+                    <TextField name="Name" label="Name" variant="standard" className="w-[500px]"
+                    fullWidth  /> <br />
+                    <TextField name="Email" label="Email" variant="standard" className="w-[500px] h-[50px]"
+                    fullWidth  /> <br />
+                    <TextField name="Password" label="Password" variant="standard" className="w-[500px] h-[75px]"
+                    fullWidth  />
+                    <TextField name="Tel." label="Tel." variant="standard" className="w-[500px] h-[50px]"
+                    fullWidth  /> <br />
+
+                    
+                    
+                    
+                    <button className="block rounded-md px-3 py-2
+                    text-white shadow-sm w-[100%] my-2
+                    bg-gradient-to-r from-[#4AC9FF] to-[#4AC9FF]
+                    hover:from-[#4AC9FF] hover:to-[#4AC9FF] transition duration-500 h-[50px] ease-in-out hover:scale-105"
+                    name="Book Venue">
+                        Register
+                    </button>
+                </div>
+            </div>
+            
+        </div>)}
