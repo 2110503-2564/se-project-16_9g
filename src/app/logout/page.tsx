37317687e4afa2d24898c1c2a9931af97d60a451
Fook-kiez <pornpanitphanpusit@gmail.com>
2025-03-24 17:06:41 +0700

    logout

---
+++ src/app/logout/page.tsx
@@ -0,0 +1,26 @@
+import DateReserve from "@/components/DateReserve"
+import { TextField, FormControl, InputLabel, Select, MenuItem } from "@mui/material"
+import { Dayjs } from "dayjs"
+import Link from "next/link"
+export default function LoginPage() {
+    return (
+<div className="font-mono flex flex-col items-center ">
+            
+            <hr className="w-[100%] " />
+            <div className="text-center flex flex-col items-center justify-center   
+            border-[#4AC9FF] border-2 shadow-lg rounded-xl p-10 w-[400px] h-[400px] mx-auto my-10 ">
+                <h1 className="text-5xl py-5 font-medium h-[100px] bg-gradient-to-r from-[#4AC9FF] to-[#4AC9FF] 
+                inline-block bg-clip-text text-transparent  ">Logout</ h1>
+                <div className="flex flex-col items-center p-5">
+                    <h1 className="text-2xl h-[70px]"> are u want to Logout</h1>
+                    <button className="block rounded-md px-3 py-2
+                    text-white shadow-sm w-[100%] my-2
+                    bg-gradient-to-r from-[#4AC9FF] to-[#4AC9FF]
+                    hover:from-[#4AC9FF] hover:to-[#4AC9FF] transition duration-500 h-[50px] ease-in-out hover:scale-105"
+                    name="Book Venue">
+                        Logout
+                    </button>   
+                </div>
+            </div>
+            
+        </div>)}
