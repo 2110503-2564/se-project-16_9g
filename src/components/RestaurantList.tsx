a08b5861189a1bf9f0f7b6d894c5c7af107f0505
nuna101 <nunajidapanak@gmail.com>
2025-03-25 18:44:35 +0700

    test edit

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -11,13 +11,12 @@ export default function RestaurantList({restaurantData} : {restaurantData:Restau
             <div style={{margin:"20px", display:"flex", flexDirection:"row", justifyContent:"space-around"}}>
             {
                 restaurantData.map((resItem:RestaurantItem) => (
-                    // <Link href={/restaurants/${resItem.id}?id=${resItem.id}} key={resItem.id}>
-                        <Card name={resItem.name} address={resItem.address} district={resItem.district} 
-                        province={resItem.province} postalcode={resItem.postalcode} picture={resItem.picture}
-                        tel={resItem.tel} region={resItem.region} opentime={resItem.opentime} 
-                        closetime={resItem.closetime} reviews={resItem.reviews} reservations={resItem.reservations}
-                        _id={""} id={resItem.id} __v={resItem.__v} key={resItem._id}/>
-                    // </Link>
+                    <Card name={resItem.name} address={resItem.address} district={resItem.district} 
+                    province={resItem.province} postalcode={resItem.postalcode} picture={resItem.picture}
+                    tel={resItem.tel} region={resItem.region} opentime={resItem.opentime} 
+                    closetime={resItem.closetime} reviews={resItem.reviews} reservations={resItem.reservations}
+                    _id={""} id={resItem.id} __v={resItem.__v} key={resItem._id}/>
+                
                 ))
             }
         </div>
